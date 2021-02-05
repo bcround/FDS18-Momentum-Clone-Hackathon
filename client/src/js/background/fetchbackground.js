1d640086aad4cc483f64c0e8f85f9e3e391ea2d7
younho <nnyyhh9247@gmail.com>
2021-02-05 14:32:44 +0900

    fixed background styling

--- client/src/js/background/fetchbackground.js
+++ client/src/js/background/fetchbackground.js
@@ -6,8 +6,7 @@ const collectionId = '27627403';
 
 const getBackgroundImage = async () => {
   try {
-    const res = await axios.get(`https://api.unsplash.com/photos/random/?collections=${collectionId}
-  &q=99&fm=jpg&crop=entropy&w=2048&cs=tinysrgb&fit=max&client_id=${accessKey}`);
+    const res = await axios.get(`https://api.unsplash.com/photos/random/?collections=${collectionId}&q=99&fm=jpg&crop=entropy&w=2048&cs=tinysrgb&fit=max&client_id=${accessKey}`);
     renderBackground(res.data);
   } catch (error) {
     console.log(error);
