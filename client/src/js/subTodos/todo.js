1e17d94cddfee618a2e72620e62a3351211b9c56
sofresh <sofreshian@gmail.com>
2021-02-05 10:39:07 +0900

    feat: removeTodo part completed

--- client/src/js/subTodos/todo.js
+++ client/src/js/subTodos/todo.js
@@ -1,11 +1,13 @@
 import getTodos from './getTodos';
 import showTodo from './showTodo';
 import addTodo from './addTodo';
+import removeTodo from './removeTodo';
 
 const allTodos = () => {
   getTodos();
   showTodo();
   addTodo();
+  removeTodo();
 };
 
 export default allTodos;
