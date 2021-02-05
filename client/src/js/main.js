a293954093ece62ae138cb39ce84e2f61fa1962b
bcround <sbc1142@gmail.com>
2021-02-05 13:12:19 +0900

    feat: Complete showing real time in the middle

--- client/src/js/main.js
+++ client/src/js/main.js
@@ -2,10 +2,12 @@ import background from './background/background';
 import weather from './weather/weather';
 import name from './name/name';
 import allTodos from './subTodos/todo';
+import mainFocus from './mainFocus/mainFocus';
 
 window.onload = () => {
   allTodos();
   weather();
   name();
   background();
+  mainFocus();
 };
