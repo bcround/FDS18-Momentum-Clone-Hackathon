ffb8ea33f96dd5a0b37b9caf7a26ef21fed76e57
sofresh <sofreshian@gmail.com>
2021-02-05 11:07:54 +0900

    feat: toggleTodo part completed

--- client/src/js/subTodos/todo.js
+++ client/src/js/subTodos/todo.js
@@ -2,12 +2,14 @@ import getTodos from './getTodos';
 import showTodo from './showTodo';
 import addTodo from './addTodo';
 import removeTodo from './removeTodo';
+import toggleTodo from './toggleTodo';
 
 const allTodos = () => {
   getTodos();
   showTodo();
   addTodo();
   removeTodo();
+  toggleTodo();
 };
 
 export default allTodos;
