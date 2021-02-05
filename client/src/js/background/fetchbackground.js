1bf04a5026f5ba0569670afa48b9f09207dc67ed
bcround <sbc1142@gmail.com>
2021-02-05 16:04:05 +0900

    docs: Write README.MD

--- client/src/js/background/fetchbackground.js
+++ client/src/js/background/fetchbackground.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 import renderBackground from './renderbackground';
 
-const accessKey = 'kwo6xgTBJqbOMSYSQz1Zo_QRSn3DIxaHLScftTUTNww';
+const accessKey = 'd-wkOuR8GwWgBI9igJ1dZrkntSsHn-wNT2VZYkLtUgM';
 const collectionId = '27627403';
 
 const getBackgroundImage = async () => {
   try {
-    const res = await axios.get(`https://api.unsplash.com/photos/random/?collections=${collectionId}&q=99&fm=jpg&crop=entropy&w=2048&cs=tinysrgb&fit=max&client_id=${accessKey}`);
+    const res = await axios.get(
+      `https://api.unsplash.com/photos/random/?collections=${collectionId}&q=99&fm=jpg&crop=entropy&w=2048&cs=tinysrgb&fit=max&client_id=${accessKey}`
+    );
     renderBackground(res.data);
   } catch (error) {
     console.log(error);
