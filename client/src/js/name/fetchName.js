9fecaa3fe8c642d0f1be1edae6a11bdf31cfa908
bcround <sbc1142@gmail.com>
2021-02-05 11:15:32 +0900

    feat: complete fetch name from server

--- client/src/js/name/fetchName.js
+++ client/src/js/name/fetchName.js
@@ -1,3 +1,9 @@
-const fetchName = () => {};
+import axios from 'axios';
+import nameEdit from './nameEdit';
+
+const fetchName = async () => {
+  const res = axios.get('http://localhost:9000/name');
+  nameEdit(res.data);
+};
 
 export default fetchName;
